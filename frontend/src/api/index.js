f4f2fb442636c7c7827548452b19bdf8c615555a
Liangzhenkun <zhenkun_liang@163.com>
2025-03-10 02:30:20 +0800

    修改端口

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000
+  baseURL: import.meta.env.VITE_APP_API_URL,
+  timeout: 10000,
+  withCredentials: true
 })
 
 // 请求拦截器
